15a82d514c72afc6f137f8fd9f261bf65cd21189
Rubén Perea <perea7r@gmail.com>
2022-05-12 10:05:22 +0200

    changes

--- src/profesor.php
+++ src/profesor.php
@@ -1,35 +1,36 @@
 <?php
 Namespace ITEC\PRESENCIAL\DAW\EXAMEN;
+use ITEC\PRESENCIAL\DAW\EXAMEN\fecha;
 
 class profesor{
-    private int $DNI;
+    private int $ID;
     private string $nombre;
-    private \Datetime $fecha;
+    private fecha $fecha;
 
-    public function __construct(int $DNI, string $nombre, int $fecha){
+    public function __construct(int $ID, string $nombre, fecha $fecha){
         $this->nombre = $nombre;
         $this->fecha = $fecha;
-        $this->DNI = $DNI;
+        $this->ID = $ID;
     }
 
-    public function crearProfesor(int $DNI, string $nombre, $fecha){
-        return new pregunta($DNI, $nombre, $fecha);
+    public function crearProfesor(int $ID, string $nombre, fecha $fecha){
+        return new profesor($ID, $nombre, $fecha);
     }
 
-    public function DNI():int {
-        return $this ->DNI;
+    public function ID():string {
+        return $this ->ID;
     }
 
     public function nombre():string {
         return $this ->nombre;
     }
 
-    public function obtenerFecha(){
-        return $this->fecha->format("j/n/Y");
+    public function createFecha(string $nombre, fecha $fecha, int $ID){
+        return new profesor($ID, $nombre, $fecha);
     }
 
-    public function isThisDNI(int $DNI) :bool{
-        return $this->DNI === $DNI;
+    public function isThisID(int $ID) :bool{
+        return $this->ID === $ID;
     }
 }
 ?>
