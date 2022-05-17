50a4a712059ebb59222f2401b0cf70d76a6f565f
Rubén Perea <perea7r@gmail.com>
2022-05-17 09:10:14 +0200

    class exam

--- src/examen.php
+++ src/examen.php
@@ -8,8 +8,15 @@ class examen{
     private listadoPreguntas $listadoPreguntas;
 
     public function __construct(asignatura $asignatura, fecha $fecha, hora $hora, profesor $profesor, listadoPreguntas $listadoPreguntas ){
-        
+        $this->fecha = $fecha;
+        $this->asignatura = $asignatura;
+        $this->hora = $hora;
+        $this->profesor = $profesor;
+        $this->listadoPreguntas = $listadoPreguntas;
     }
-        
+    
+    public static function creaExamen(asignatura $asignatura, fecha $fecha, hora $hora, profesor $profesor, listadoPreguntas $listadoPreguntas){
+        return new examen($asignatura, $fecha, $hora, $profesor, $listadoPreguntas);
+    }
 }
 ?>
